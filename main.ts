f33e5230667b124d254f06c70209012566caa8af
Petr Kredvík <petr.kredvik.025@pslib.cz>
2026-05-04 11:41:20 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,44 @@
 // ReactionGame – instrukce v README.md
-basic.showIcon(IconNames.Happy)
+let dobaCekani = randint(3,6)
+
+function presypaciHodiny() {
+basic.clearScreen()
+    led.plot(0, 0)
+    led.plot(1, 0)
+    led.plot(2, 0)
+    led.plot(3, 0)
+    led.plot(4, 0)
+    led.plot(2, 1)
+    led.plot(1, 2)
+    led.plot(2, 2)
+    led.plot(3, 2)
+    led.plot(2, 3)
+    led.plot(0, 4)
+    led.plot(1, 4)
+    led.plot(2, 4)
+    led.plot(3, 4)
+    led.plot(4, 4)
+}
+input.onButtonPressed(Button.AB, function(){
+    presypaciHodiny()
+    music.playTone(340, 500)
+    basic.pause(dobaCekani * 1000)
+})
+
+function viteznyTonA() {
+    music.playTone(4000,500)
+}
+function viteznyTonB() {
+    music.playTone(2000,500)
+}
+if (input.buttonIsPressed(Button.A)) {
+    viteznyTonB()
+}
+if (input.buttonIsPressed(Button.B)) {
+    viteznyTonA()
+}
+if (input.buttonIsPressed(Button.AB)) {
+    music.playTone(500, 250)
+    music.playTone(250, 250)
+    basic.showIcon(IconNames.Sad)
+}
